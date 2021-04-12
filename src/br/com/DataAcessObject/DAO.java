19fea651072d5368eddc2fd040c9074dc62a8669
Jack Pastori <jo_pastori@hotmail.com>
2021-04-12 15:36:38 -0300

    Update DAO.java

--- src/br/com/DataAcessObject/DAO.java
+++ src/br/com/DataAcessObject/DAO.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-//import java.util.ArrayList;
-//import java.util.List;
 import java.util.List;
 
 import Farmacia.Usuario;
